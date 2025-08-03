d8ab85787356ddbbc71905e4a276fa1c02a7729d
Moritz344 <moritzmaier34@gmail.com>
2025-08-03 18:54:10 +0200

    displaying chat and input with box

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const { search, Separator, select, confirm, input, password, checkbox } = require('@inquirer/prompts');
 const mineflayer = require('mineflayer');
 const chalk = require('chalk').default;
+var blessed = require('blessed');
+const fs = require('fs');
+const path = require('path');
 
 // TODO: react to mc chatgames / solve math games // Linux dic: fs.readFileSync('/usr/share/dict/words')
 // TODO: save server cahts in file
@@ -8,7 +11,82 @@ const chalk = require('chalk').default;
 
 
 const operators = ['+', '-', '*', '/'];
-const solveMathQuestions = false;
+// get json data
+const configPath = path.resolve(__dirname, 'config.json');
+const configData = fs.readFileSync(configPath, 'utf8');
+const config = JSON.parse(configData);
+
+const solveMathQuestions = config.chatInteraction.solveMathProblems;
+const solveMathTimeout = config.chatInteraction.solveMathTimeout;
+const sendMathResult = config.chatInteraction.sendAnswerAutomatically;
+const ignoreMyMessages = config.chatInteraction.IgnoreOwnMessages;
+
+
+
+var screen = blessed.screen({
+  smartCSR: true,
+  title: 'Mineflayer Bot'
+});
+
+var box = blessed.box({
+  top: 'top',
+  left: 'center',
+  width: '100%',
+  height: '95%',
+  content: 'Connecting to the server...',
+  scrollable: true,
+  alwaysScroll: true,
+  vi: true,
+  border: {
+    type: 'line',
+    fg: 'cyan'
+  },
+  style: {
+    fg: 'white',
+    bg: 'black',
+    border: {
+      fg: 'cyan'
+    }
+  }
+});
+
+
+var icon = blessed.box({
+  top: '0',
+  left: '0',
+  width: '1',
+  height: '1',
+  content: '🤖',
+  style: {
+    fg: 'white',
+    bg: 'black'
+  }
+});
+
+box.focus();
+
+screen.render();
+
+var inputBox = blessed.textbox({
+  bottom: '0',
+  left: '0',
+  width: '100%',
+  height: 'shrink',
+  inputOnFocus: true,
+  style: {
+    fg: 'white',
+    bg: 'black',
+    border: {
+      fg: 'cyan'
+    }
+  }
+});
+
+screen.append(inputBox);
+screen.append(icon);
+screen.append(box);
+
+inputBox.focus();
 
 async function main() {
   console.clear();
@@ -18,101 +96,55 @@ async function main() {
   const testVersion = "1.8.9";
   const testAuth = "microsoft";
 
-  if (!process.argv.length > 2) {
 
-    console.log(process.argv.length);
-
-
-    const server = await input({
-      message: 'Enter the Minecraft server IP:',
-      validate: (value) => {
-        return value ? true : 'Server IP cannot be empty';
-      }
-
-
-    });
-    const username = await input({
-      message: 'Enter the bot username:',
-      validate: (value) => {
-        return value ? true : 'Username cannot be empty';
-      }
-    });
-
-    const versionNumber = await input({
-      message: 'Enter the Minecraft version:',
-      validate: (value) => {
-        return value ? true : 'Version cannot be empty';
-      }
-    })
-
-    const authType = await select({
-      message: 'Select authentication type:',
-      choices: [
-        { name: 'Microsoft Account', value: 'microsoft' },
-      ],
-      validate: (value) => {
-        return value ? true : 'Authentication type cannot be empty';
-      }
-    });
-    const bot = mineflayer.createBot({
-      host: server  ,
-      username: username ,
-      auth: authType  ,
-      version: versionNumber  ,
-    });
-    console.log('Connecting to the server...');
-    await SpawnBot(bot);
-    await ListenForChat(bot);
-    await ListenForErrors(bot);
-  }else {
-    console.log('Connecting to the server...');
     const bot = mineflayer.createBot({
       host: process.argv[2],
       username: process.argv[3],
-      auth: process.argv[4] ,
-      version: process.argv[5] ,
+      auth: process.argv[4],
+      version: process.argv[5],
     });
+    console.log('Connecting to the server...');
     await SpawnBot(bot);
     await ListenForChat(bot);
     await ListenForErrors(bot);
-  }
-
-
-
-
-
-
-
 
 }
 
 main();
 
 
+function SendNotification(message) {
+  box.pushLine(`${chalk.green('Notification')}: ${message}`);
+  screen.render();
+}
 
 
 async function SpawnBot(bot) {
   return new Promise((resolve, reject) => {
     bot.on('spawn', () => {
-      console.log(`Bot ${bot.username} has spawned in the game.`);
+      SendNotification('Bot has spawned in the game.');
       resolve();
     });
+
 
     bot.on('error', (err) => {
       console.error('Error occurred:', err);
     });
 
-    // show reason why bot disconnected
 
     bot.on('end', (reason) => {
-      console.log('Bot has disconnected from the server.');
-      console.log("Reson:", reason);
+      console.log(chalk.red.bold('Bot has disconnected from the server.'));
+      console.log("Reason:", reason);
+      setTimeout(() => {
+        process.exit(0);
+      }, 5000);
+
+
     });
   });
 
 }
 
-//SpawnBot(bot);
 
 
 
@@ -124,7 +156,12 @@ function solveMathQuestion(question,bot) {
       if (question.includes(operator)) {
         let match = question.match(/\d+\s*[\+\-\*\/]\s*\d+/);
         var result = eval(match[0]);
-        bot.chat(result.toString());
+        if (sendMathResult) {
+          setTimeout(() => {
+            SendChatMessage(bot, result);
+          }, solveMathTimeout);
+        }
+        SendNotification(`Solved math question: ${chalk.green.bold(match[0])} = ${chalk.yellow.bold(result)}`);
       }
 
     }
@@ -132,64 +169,71 @@ function solveMathQuestion(question,bot) {
 
 
 }
+
+async function SendChatMessage(bot,message) {
+   box.pushLine(`${chalk.yellow(bot.username)}: ${message}`);
+   bot.chat(message);
+   inputBox.focus();
+   inputBox.clearValue();
+   screen.render();
+
+}
+
+function ListenForMathQuestions(bot,message) {
+      if (solveMathQuestions) {
+        try {
+          solveMathQuestion(message,bot);
+        }catch (error) {
+          SendNotification(`Error solving math question: ${chalk.red.bold(error.message)}`);
+        }
+
+      }
+}
+
 async function ListenForChat(bot) {
 
   bot.on('chat', (username, message) => {
-    if (message === 'gg') {
-      bot.chat('gg');
-    }else if (message === "o/" ) {
-      bot.chat('o/');
-    }else if (message === "gn") {
-      bot.chat('gn');
+    //if (message === 'gg') {
+    //  SendChatMessage(bot, 'gg');
+    //}else if (message === "o/" ) {
+    //  SendChatMessage(bot, 'o/');
+    //}else if (message === "gn") {
+    //  SendChatMessage(bot, 'gn');
+    //}
+
+    if (ignoreMyMessages ) {
+      if (username === bot.username) {
+        ListenForMathQuestions(bot,message);
+        return; // Ignore own messages
+      };
     }
-
-    if (solveMathQuestions) {
-      try {
-        solveMathQuestion(message,bot);
-      }catch (error) {
-        console.error('Error solving math question:', error);
-      }
-
-    }
+      box.pushLine(`${chalk.blue(username)}: ${message}`);
+      box.scroll(1.0);
+      screen.render();
 
 
 
-    console.log(chalk.yellow(`${username}` + chalk.white(': ') + chalk.green(message)));
-
+      ListenForMathQuestions(bot,message);
 
   });
 
-  try {
-
-  while (true) {
-    const userMessage = await input({
-      message: '',
-      validate: (value) => {
-        return value ? true : 'Message cannot be empty';
-      }
-    });
-
-    if (userMessage.toLowerCase() === 'exit') {
-      bot.quit();
-      process.exit(0);
+  inputBox.on('submit', (value) => {
+    let message = value.trim();
+    SendChatMessage(bot,message);
+    if (value.trim() === "exit") {
+          process.exit(0);
     }
 
+    });
 
-    console.log(chalk.blue(`You: ${userMessage}`));
-    bot.chat(userMessage);
-  }
-
-  } catch (error) {
-    console.error(chalk.red('Exited Prompt. You can no longer chat'));
-  }
 
 }
 
-//ListenForChat();
 
 async function ListenForErrors(bot) {
   bot.on('kicked', (reason) => {
-    console.error('Kicked from the server:', reason);
+    SendNotification(`Kicked from the server: ${reason}`);
   });
 
 }
+
